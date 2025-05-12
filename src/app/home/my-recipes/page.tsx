f5179c95c45b957eed97cb33b4af3bcaec9e36aa
Justin-K-Ellis <jlke45@gmail.com>
2025-05-12 22:22:36 +0900

    feat: create pages/routes for pages

---
+++ src/app/home/my-recipes/page.tsx
@@ -0,0 +1,9 @@
+import PageTitle from "@/app/components/PageTitle";
+
+export default function page() {
+  return (
+    <>
+      <PageTitle text="My Recipes" />
+    </>
+  );
+}
---
+++ src/app/home/want-to-make/page.tsx
@@ -0,0 +1,5 @@
+import PageTitle from "@/app/components/PageTitle";
+
+export default function page() {
+  return <PageTitle text="Want to Make" />;
+}
