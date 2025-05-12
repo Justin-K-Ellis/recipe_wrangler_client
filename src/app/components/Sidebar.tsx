b3379cb2d2a9cf4a5bd0bd173c7784022d88f0f1
Justin-K-Ellis <jlke45@gmail.com>
2025-05-12 15:21:05 +0900

    feat: create Sidebar component

---
+++ src/app/components/Sidebar.tsx
@@ -0,0 +1,37 @@
+import Link from "next/link";
+import { SideBarItem } from "../types";
+
+export default function Sidebar() {
+  const listItems: SideBarItem[] = [
+    {
+      name: "Favorites",
+      icon: "❤️",
+      link: "/favorites",
+    },
+    {
+      name: "Interested",
+      icon: "🤔",
+      link: "/interested",
+    },
+    {
+      name: "My Recipes",
+      icon: "✍️",
+      link: "/my-recipes",
+    },
+  ];
+
+  return (
+    <div className="w-2/10 h-full mx-2 shadow p-2">
+      <ul className="flex flex-col gap-4">
+        {listItems.map((item) => (
+          <li
+            key={item.name}
+            className="text-xl border border-accent rounded shadow p-1 hover:bg-base-300"
+          >
+            <Link href={item.link}>{item.name + " " + item.icon}</Link>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
