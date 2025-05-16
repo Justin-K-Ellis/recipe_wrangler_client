71e8240a4cb67feb24513f83ec023b34a0cc2c0c
Justin-K-Ellis <jlke45@gmail.com>
2025-05-16 09:14:10 +0900

    feat: create RecipeFullInfo & RecipePreview interfaces

--- src/app/types.ts
+++ src/app/types.ts
@@ -1,7 +1,23 @@
+interface RecipeFullInfo {
+  id: number;
+  name: string;
+  ingredients: string[];
+  steps: string[];
+  readyInMinutes: number;
+  servings: number;
+}
+
+interface RecipePreview {
+  uuid: string;
+  name: string;
+  cuisine: string;
+  notes: string;
+}
+
 interface SideBarItem {
   name: string;
   icon: string;
   link: string;
 }
 
-export type { SideBarItem };
+export type { RecipeFullInfo, RecipePreview, SideBarItem };
