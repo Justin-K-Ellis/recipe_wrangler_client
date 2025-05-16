cf1e433e89c8a47435aab3520e961b90a6641fcf
Justin-K-Ellis <jlke45@gmail.com>
2025-05-16 12:29:43 +0900

    fix: Update about page

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,5 +1,13 @@
 import PageTitle from "../components/PageTitle";
 
 export default function page() {
-  return <PageTitle text="About" />;
+  return (
+    <>
+      <PageTitle text="About" />
+      <p>
+        Recipe Wrangler helps you find and use delicious recipes and create and
+        manage your own.
+      </p>
+    </>
+  );
 }
