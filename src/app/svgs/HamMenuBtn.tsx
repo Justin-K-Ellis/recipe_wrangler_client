3741085f8721cdafb0ba890bffba0579c6d416b4
Justin-K-Ellis <jlke45@gmail.com>
2025-06-22 22:40:45 +0900

    feat: create hamburger SVG icon

---
+++ src/app/svgs/HamMenuBtn.tsx
@@ -0,0 +1,11 @@
+export default function HamMenuBtn() {
+  return (
+    <div>
+      <svg viewBox="0 0 100 80" width="25" height="25">
+        <rect width="100" height="20"></rect>
+        <rect y="30" width="100" height="20"></rect>
+        <rect y="60" width="100" height="20"></rect>
+      </svg>
+    </div>
+  );
+}
