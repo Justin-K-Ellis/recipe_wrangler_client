8ee9874388d511b088aa8ddca8bf77a3f3e4585a
Justin-K-Ellis <jlke45@gmail.com>
2025-06-23 14:26:04 +0900

    feat: display all sidebar content in hamburger menu

--- src/app/components/HamburgerMenu.tsx
+++ src/app/components/HamburgerMenu.tsx
@@ -1,14 +1,33 @@
+import Link from "next/link";
 import listItems from "../data/sideBarData";
 import SignInSignOutBtn from "./SignInSignOutBtn";
-import RegisterBtn from "./RegisterBtn";
+import SidebarBtn from "./SidebarBtn";
 
-export default function HamburgerMenu() {
-  console.log(listItems);
+interface HamburgerMenuProps {
+  isSignedIn: boolean;
+}
 
+export default function HamburgerMenu({ isSignedIn }: HamburgerMenuProps) {
   return (
-    <nav className="w-full md:hidden">
+    <nav className="w-full md:hidden mb-1">
       <ul className="w-full">
-        <li>Test</li>
+        {listItems.map((item) => (
+          <li
+            className="flex flex-row justify-center border-b-1 border-primary text-2xl"
+            key={item.name}
+          >
+            <Link href={item.link}>{item.name}</Link>
+          </li>
+        ))}
+        <li className="p-1 flex flex-row justify-center items-center border-b-1 border-primary">
+          <SidebarBtn link="/home/search" text="Find a Recipe" />
+        </li>
+        <li className="p-1 flex flex-row justify-center items-center border-b-1 border-primary">
+          <SidebarBtn link="/home/create" text="Create a Recipe" />
+        </li>
+        <li className=" p-1 flex flex-row justify-center border-b-1 border-primary">
+          <SignInSignOutBtn isSignedIn={isSignedIn} />
+        </li>
       </ul>
     </nav>
   );
--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -7,9 +7,11 @@ import SignInSignOutBtn from "./SignInSignOutBtn";
 import RegisterBtn from "./RegisterBtn";
 import auth from "../auth/firebase.js";
 import HamburgerMenu from "./HamburgerMenu";
+import HamMenuBtn from "../svgs/HamMenuBtn";
 
 export default function Header() {
   const [isSignedIn, setIsSignedIn] = useState(false);
+  const [showHamburgerMenu, setShowHamburgerMenu] = useState(false);
 
   useEffect(() => {
     onAuthStateChanged(auth, (user) => {
@@ -24,19 +26,34 @@ export default function Header() {
     });
   }, []);
 
+  function toggleHamburgerMenu(): void {
+    if (showHamburgerMenu) {
+      setShowHamburgerMenu(false);
+    } else {
+      setShowHamburgerMenu(true);
+    }
+  }
+
   return (
-    <nav className="navbar bg-base-100 shadow-sm mb-4">
-      <Link href={"/"} className="md:text-2xl font-bold navbar-start">
-        Recipe Wrangler
-      </Link>
-      <div className="navbar-end md:flex md:flex-row hidden gap-2">
-        <button className="btn btn-soft btn-primary">
-          <Link href={"/about"}>About</Link>
-        </button>
-        <SignInSignOutBtn isSignedIn={isSignedIn} />
-        {!isSignedIn && <RegisterBtn />}
-        {isSignedIn && <HamburgerMenu />}
-      </div>
-    </nav>
+    <>
+      <nav className="navbar bg-base-100 shadow-sm mb-4 flex flex-row justify-between">
+        <Link href={"/"} className="md:text-2xl font-bold navbar-start">
+          Recipe Wrangler
+        </Link>
+        <div className="navbar-end md:flex md:flex-row hidden gap-2">
+          <button className="btn btn-soft btn-primary">
+            <Link href={"/about"}>About</Link>
+          </button>
+          <SignInSignOutBtn isSignedIn={isSignedIn} />
+          {!isSignedIn && <RegisterBtn />}
+        </div>
+        <div onClick={toggleHamburgerMenu} className="md:hidden">
+          {isSignedIn && <HamMenuBtn />}
+        </div>
+      </nav>
+      {isSignedIn && showHamburgerMenu && (
+        <HamburgerMenu isSignedIn={isSignedIn} />
+      )}
+    </>
   );
 }
