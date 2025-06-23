b6726ae27842428d71604f41278a64dfe3c62ea8
Justin-K-Ellis <jlke45@gmail.com>
2025-06-23 23:14:45 +0900

    fix: disable unused sidebar data until it's used

--- src/app/data/sideBarData.tsx
+++ src/app/data/sideBarData.tsx
@@ -1,16 +1,16 @@
 import { SideBarItem } from "../types";
 
 const listItems: SideBarItem[] = [
-  {
-    name: "Favorites",
-    icon: "❤️",
-    link: "/home/favorites",
-  },
-  {
-    name: "Want to Make",
-    icon: "🤔",
-    link: "/home/want-to-make",
-  },
+  // {
+  //   name: "Favorites",
+  //   icon: "❤️",
+  //   link: "/home/favorites",
+  // },
+  // {
+  //   name: "Want to Make",
+  //   icon: "🤔",
+  //   link: "/home/want-to-make",
+  // },
   {
     name: "My Recipes",
     icon: "✍️",
