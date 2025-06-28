fd1ef5c0634f0de6083ea430a2b0708e7f265a9f
Justin-K-Ellis <jlke45@gmail.com>
2025-06-28 14:23:00 +0900

    feat: create hook to get custom recipe data

---
+++ hooks/useGetCustomRecipeData.tsx
@@ -0,0 +1,38 @@
+import { useState, useEffect } from "react";
+import type { RecipeFullInfo } from "@/app/types";
+
+export default function useGetCustomRecipeData(
+  recipeId: string
+): RecipeFullInfo | null {
+  const [recipeData, setRecipeData] = useState<RecipeFullInfo>({
+    externalId: 0,
+    name: "",
+    ingredients: [],
+    steps: [],
+    readyInMinutes: 0,
+    servings: 0,
+  });
+  const api = process.env.NEXT_PUBLIC_EXP_API;
+
+  useEffect(() => {
+    async function getCustomRecipeData() {
+      if (!recipeId) {
+        return null;
+      }
+      const response = await fetch(`${api}/custom-recipe/${recipeId}`);
+      if (!response.ok) {
+        throw new Error("Could not fetch recipe");
+      } else {
+        const data: RecipeFullInfo = await response.json();
+        setRecipeData(data);
+      }
+    }
+    getCustomRecipeData();
+  }, [api, recipeId]);
+
+  if (!recipeId) {
+    return null;
+  }
+
+  return recipeData;
+}
